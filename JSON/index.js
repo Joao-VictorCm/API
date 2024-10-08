988eb04f0929860767ebb71b725e3dc6d07b3119
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-10-07 22:17:55 -0300

    Incluido o json no metodo GET

--- JSON/index.js
+++ JSON/index.js
@@ -4,23 +4,37 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 1: Run the solution.js file without looking at the code.
-//Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
+
+//Etapa 1: execute o arquivo Solution.js sem olhar o código.
+//Etapa 2: Você pode acessar o arquivo Recipe.json para ver a estrutura completa do RecipeJSON abaixo.
+
 const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
 
-app.use(express.static("public"));
+
+  app.use(
+    express.static("public", {
+      setHeaders: (res, path) => {
+        if (path.endsWith(".css")) {
+          res.set("Content-Type", "text/css");
+        }
+      },
+    })
+  );
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  const data = JSON.parse(recipeJSON)
+  console.log(data)
+  res.render("index.ejs", data);
 });
 
 app.post("/recipe", (req, res) => {
-  //Step 3: Write your code here to make this behave like the solution website.
-  //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+//Etapa 3: Escreva seu código aqui para que ele se comporte como o site da solução.
+//Etapa 4: Adicione código a views/index.ejs para usar o objeto de receita recebido.
+
 });
 
-app.listen(port, () => {
+app.listen(port, () => { 
   console.log(`Server running on port: ${port}`);
 });
